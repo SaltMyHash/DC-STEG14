d3b19af0a90e887cceef1c3a0f7318b15a075224
SaltMyHash <49442663+SaltMyHash@users.noreply.github.com>
2019-04-11 13:57:08 -0400

    Add files via upload

---
+++ So you found a stego file....mm
@@ -0,0 +1,22 @@
+<map version="1.0.0"><node ID="B264D02C-0A45-48E6-964E-E1EFCC5F32F1" BACKGROUND_COLOR="#FFFFFF" TEXT="Stego file" COLOR="#1A5469" POSITION="right" STYLE="bubble"><edge COLOR="#1A5469" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="24" /><node ID="F7C98DBE-1E82-4FE8-AC0B-E8A14DD668EE" TEXT="PNG" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="999A4144-337F-469E-8890-E079A67B7947" TEXT="rSteg" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="E3701220-C2B5-492D-974E-E4E93770B695" TEXT="Image Steganography" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="CD6F8969-7441-449C-8A19-B257264394DB" TEXT="SSuite Picsel" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="0DB3D8EE-5929-4543-B149-F1D16C4AAAE7" TEXT="Have original image" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="F07003CC-D1C8-4440-8010-623B9E6F3A20" TEXT="Need original image" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="7F747B5F-E0E1-4266-89B5-0A577980C26E" TEXT="Open Stego" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="BAF9299D-E507-4CEC-BE71-89487918B151" TEXT="JPEG" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="E711553B-87CA-425D-8031-589BCA9A26C6" TEXT="Hide&#8217;n&#8217;Send" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="2965555A-9014-4CF3-A313-390003C8D320" TEXT="SteganPEG" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="43916FED-937F-454E-A174-D4E91A8A083A" TEXT="SilentEye" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="6E031840-6418-43FC-84BB-82409841DCAE" TEXT="PDF" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="85CA55B9-6F4F-4ED5-A776-7211FBA27476" TEXT="" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="05F25D88-AEFB-4038-B26A-409DBB66272C" TEXT="" COLOR="#435B66" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="E10CCECC-C728-4DBF-A5D8-160C98681899" TEXT="BMP" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="574841D8-978C-4598-818A-C4D93AFD1C4F" TEXT="Silent Eye" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="215C6E07-9177-415A-86C4-D2B7AC26CFF1" TEXT="Xaio Steganography" COLOR="#435A66" POSITION="left" STYLE="fork"><edge COLOR="#389237" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="D5E3D2F9-E0AC-4F7A-8574-DC013C8BB38C" TEXT="Wav" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#DC2128" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="BEA5BF2C-DB40-41A0-859F-3645A8BDFCF4" TEXT="" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#DC2128" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="421B06C3-40AC-4C62-967E-11206CC13CCC" TEXT="SilentEye" COLOR="#435B66" POSITION="left" STYLE="fork"><edge COLOR="#DC2128" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+</map>
